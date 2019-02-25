fe3920ad419ff1564645585e685aaa3ee83847dd
sshhane <g00332592@gmit.ie>
2019-02-25 11:41:02 +0000

    back to old

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ActiveRecord::Base
-    def self.ratings
-        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
-    end
+    def self.all_ratings
+	      result = {}
+	  	  self.select(:rating).uniq.each do |movie|
+	  		result[movie.rating] = 1
+		  end
+	  	  return result
+	    end
 end
